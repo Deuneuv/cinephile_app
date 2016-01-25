b272501fbacc5b3ed1e6e87d10e0d77b01a82509
Deuneuv <makdeuneuv@gmail.com>
2016-01-25 21:02:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CinephileApp::Application.config.secret_token = '45dccdb405fb95f02948de10ae87ed9ce912f5ce41470234b621753a417e9f4e58261143e9a9be0779a577d8c30a267577f7a0764334690d2704288c500ceaeb'
