43f392fb0dba8fec2c6098a779eac79e83135c8e
Deuneuv <makdeuneuv@gmail.com>
2016-02-03 02:14:33 +0100

    Résolution des liens entre pages

--- Gemfile
+++ Gemfile
@@ -6,9 +6,11 @@ gem 'rails', '3.0.5'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', '1.3.3', :require => 'sqlite3', :group => :development
-gem 'rake', '0.9.2.2'
-gem 'heroku', '~> 3.42', '>= 3.42.31'
-gem 'rdoc'
+gem 'rake', '0.9.6'
+gem 'heroku', '3.42.31'
+gem 'rdoc', '4.1.1' 
+gem 'tasks', '1.3'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ CinephileApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
-  get "pages/help"
 
+  root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
   resources :microposts
   resources :users
-
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -58,13 +58,9 @@ CinephileApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "pages#home"
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
+
